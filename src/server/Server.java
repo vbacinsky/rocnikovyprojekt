27098a6ba98fed347c227a53c9131132426bed8c
vbacinsky <vladimir.bacinsky@gmail.com>
2020-08-16 15:03:00 +0200

    After Fix

--- src/server/Server.java
+++ src/server/Server.java
@@ -17,7 +17,7 @@ public class Server {
     private Game game;
 
 
-    public static void main(String[] args) throws IOException {
+    public void run() throws IOException {
         ServerSocket ss = new ServerSocket(22222);
         int id = 1;
 
@@ -25,7 +25,7 @@ public class Server {
             if(id > 2) break;
             Socket client = ss.accept();
             System.out.println(id  + ". client connected");
-            ClientHandler clientThread = new ClientHandler(client, id, clients, new Server());
+            ClientHandler clientThread = new ClientHandler(client, id, clients, this);
             clients.add(clientThread);
             pool.execute(clientThread);
             id++;
